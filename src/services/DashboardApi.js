cd4eecb84eb4add5ee4a9dea6e3bd74190c19520
kddove85 <kddove85@gmail.com>
2021-10-04 20:22:29 -0400

    Update DashboardApi.js

--- src/services/DashboardApi.js
+++ src/services/DashboardApi.js
@@ -1,73 +1,73 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8000/api',
+  baseURL: 'https://indicatorsdashboard.herokuapp.com/api',
   timeout: 30000,
-  headers: {'Access-Control-Allow-Origin': 'http://localhost:3000'}
+  headers: {'Access-Control-Allow-Origin': 'https://indicatorsdashboardfrontend.herokuapp.com'}
 });
 
 function getGdp() {
-  return axios.get('http://localhost:8000/api/gdp/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/gdp/').then((response) => {
     return response.data.data;
   })
 }
 
 function getCpi() {
-  return axios.get('http://localhost:8000/api/cpi/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/cpi/').then((response) => {
     return response.data.data;
   })
 }
 
 function getUnemployment() {
-  return axios.get('http://localhost:8000/api/unemployment/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/unemployment/').then((response) => {
     return response.data.data;
   })
 }
 
 function getImmigration() {
-  return axios.get('http://localhost:8000/api/immigration/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/immigration/').then((response) => {
     return response.data.data;
   })
 }
 
 function getDeportation() {
-  return axios.get('http://localhost:8000/api/deportation/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/deportation/').then((response) => {
     return response.data.data;
   })
 }
 
 function getDepartmentSpending() {
-  return axios.get('http://localhost:8000/api/spending/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/spending/').then((response) => {
     return response.data.data;
   })
 }
 
 function getGasPrices() {
-  return axios.get('http://localhost:8000/api/gas/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/gas/').then((response) => {
     return response.data.data;
   })
 }
 
 function getOrders() {
-  return axios.get('http://localhost:8000/api/orders/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/orders/').then((response) => {
     return response.data.data;
   })
 }
 
 function getDeficit() {
-  return axios.get('http://localhost:8000/api/deficit/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/deficit/').then((response) => {
     return response.data.data;
   })
 }
 
 function getInitialApproval() {
-  return axios.get('http://localhost:8000/api/initial_approval/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/initial_approval/').then((response) => {
     return response.data.data;
   })
 }
 
 function getFinalApproval() {
-  return axios.get('http://localhost:8000/api/final_approval/').then((response) => {
+  return axios.get('https://indicatorsdashboard.herokuapp.com/api/final_approval/').then((response) => {
     return response.data.data;
   })
 }
